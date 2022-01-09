a504ee756973a0920bd9da817472004d91222a68
larisa.cacovean@gmail.com <larisa.cacovean@gmail.com>
2022-01-09 14:46:32 +0200

    laborator 11

--- Cacovean_Larisa_Lab10/ListEntryPage.xaml.cs
+++ Cacovean_Larisa_Lab10/ListEntryPage.xaml.cs
@@ -1,13 +1,8 @@
 ﻿using Cacovean_Larisa_Lab10.Models;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
-using Cacovean_Larisa_Lab10.Models;
 
 namespace Cacovean_Larisa_Lab10
 {
